4ac339371529d53b4fb0b5fc384c47269f4c7e7a
namarora <namarora@adobe.com>
2025-02-07 16:07:05 +0530

    expo 52 upgraded ios build succeeded with work around of commenting out /RCTAppSetupUtils.h in pods

--- apps/AEPSampleAppNewArchEnabled/android/app/src/main/java/com/AEPSampleAppNewArchEnabled/MainApplication.kt
+++ apps/AEPSampleAppNewArchEnabled/android/app/src/main/java/com/AEPSampleAppNewArchEnabled/MainApplication.kt
@@ -23,6 +23,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 import expo.modules.ApplicationLifecycleDispatcher
@@ -52,10 +53,11 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
+          val packages = PackageList(this).packages
           override fun getPackages(): List<ReactPackage> {
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(new MyReactNativePackage());
-            return PackageList(this).packages
+            return packages
           }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
@@ -93,7 +95,7 @@ class MainApplication : Application(), ReactApplication {
     )
     MobileCore.registerExtensions(extensions, AdobeCallback { o: Any? ->
     })
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
--- apps/AEPSampleAppNewArchEnabled/ios/AEPSampleAppNewArchEnabled/AppDelegate.mm
+++ apps/AEPSampleAppNewArchEnabled/ios/AEPSampleAppNewArchEnabled/AppDelegate.mm
@@ -10,26 +10,16 @@ governing permissions and limitations under the License.
 */
 
 #import "AppDelegate.h"
-#import "AdobeBridge.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
 
-@implementation AppDelegate {
-  UIApplicationState appState;
-}
+@implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"main";
-
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  [AdobeBridge configure: application.applicationState];
-  
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -54,8 +44,7 @@ governing permissions and limitations under the License.
 
 // Universal Links
 - (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
-  BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result;
+  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 // Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
@@ -64,29 +53,14 @@ governing permissions and limitations under the License.
   return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
 }
 
-// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
   return [super application:application didFailToRegisterForRemoteNotificationsWithError:error];
 }
 
-// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
   return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
-
-
-// Setup for Lifecycle Start when entering foreground
-- (void)applicationWillEnterForeground:(UIApplication *)application{
-  appState = application.applicationState;
-  [AdobeBridge lifecycleStart];
-}
-
-// Setup for Lifecycle Start when entering background
-- (void)applicationDidEnterBackground:(UIApplication *)application{
-  appState = application.applicationState;
-  [AdobeBridge lifecyclePause];
 }
 
 @end
